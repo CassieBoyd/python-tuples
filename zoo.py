17bca7189815913d45c222f36a88863fe7bda6d5
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-14 21:01:24 -0600

    Unpacking animals

--- zoo.py
+++ zoo.py
@@ -18,3 +18,20 @@ if animal_to_find in zoo:
     print(f"Here's your {animal_to_find}!")
 else:
     print(f"We're sorry, but your {animal_to_find} escaped.")
+
+"""
+You can reverse engineer (unpack) a tuple into another tuple with the following syntax.
+children = ("Sally", "Hansel", "Gretel", "Svetlana")
+(first_child, second_child, third_child, fourth_child) = children
+print(first_child) # Output is "Sally"
+print(second_child) # Output is "Hansel"
+print(third_child) # Output is "Gretel"
+print(fourth_child) # Output is "Svetlana"
+
+Create a variable for the animals in your zoo tuple, and print them to the console.
+"""
+
+(first_animal, second_animal, third_animal, fourth_animal, fifth_animal, sixth_animal, seventh_animal, eighth_animal,ninth_animal, tenth_animal) = zoo
+
+print(third_animal)
+

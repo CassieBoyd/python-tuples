5b9a60d2aaa474db654f8c67aea2c859e09bba95
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-14 20:42:40 -0600

    Printed the index of an animal

--- zoo.py
+++ zoo.py
@@ -1,3 +1,16 @@
-# Create a tuple named zoo that contains 10 of your favorite animals.
+""" 
+Create a tuple named zoo that contains 10 of your favorite animals.
+"""
 
 zoo = ("cat", "dragon", "thestral", "unicorn", "raven", "eevee", "fox", "rabbit", "ninja turtle (any age)", "thundercat")
+
+""" 
+Find one of your animals using the tuple.index(value) syntax on the tuple.
+"""
+
+print(zoo.index("raven"))
+
+"""
+Determine if an animal is in your tuple by using value in tuple syntax.
+"""
+

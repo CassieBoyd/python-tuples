fe54009d09802ee9f307c75d31d0525446aed03d
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-14 20:33:00 -0600

    first commit

---
+++ zoo.py
@@ -0,0 +1,3 @@
+# Create a tuple named zoo that contains 10 of your favorite animals.
+
+zoo = ("cat", "dragon", "thestral", "unicorn", "raven", "eevee", "fox", "rabbit", "ninja turtle (any age)", "thundercat")
